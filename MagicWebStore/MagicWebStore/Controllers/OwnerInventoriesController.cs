0bdfd451b783fefbfe71a67f3c655de3771ca46d
Rita <rita.h.tse@gmail.com>
2018-04-25 16:23:02 +1000

    Added the OwnerInventories method

--- MagicWebStore/MagicWebStore/Controllers/OwnerInventoriesController.cs
+++ MagicWebStore/MagicWebStore/Controllers/OwnerInventoriesController.cs
@@ -19,10 +19,34 @@ namespace MagicWebStore.Controllers
         }
 
         // GET: OwnerInventories
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    var magicWebStoreContext = _context.OwnerInventory.Include(o => o.Product);
+        //    return View(await magicWebStoreContext.ToListAsync());
+        //}
+
+            //Code from WEEK 7 TUTORIAL
+        // Auto-parsed variables coming in from the request - there is a form on the page to send this data.
+        public async Task<IActionResult> Index(string productName)
         {
-            var magicWebStoreContext = _context.OwnerInventory.Include(o => o.Product);
-            return View(await magicWebStoreContext.ToListAsync());
+            // Eager loading the Product table - join between OwnerInventory and the Product table.
+            var query = _context.OwnerInventory.Include(x => x.Product).Select(x => x);
+
+            if (!string.IsNullOrWhiteSpace(productName))
+            {
+                // Adding a where to the query to filter the data.
+                // Note for the first request productName is null thus the where is not always added.
+                query = query.Where(x => x.Product.Name.Contains(productName));
+
+                // Storing the search into ViewBag to populate the textbox with the same value for convenience.
+                ViewBag.ProductName = productName;
+            }
+
+            // Adding an order by to the query for the Product name.
+            query = query.OrderBy(x => x.Product.Name);
+
+            // Passing a List<OwnerInventory> model object to the View.
+            return View(await query.ToListAsync());
         }
 
         // GET: OwnerInventories/Details/5
